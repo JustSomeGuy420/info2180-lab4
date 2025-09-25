62b8b847a3d03d0fbae4c9dd1c100a3e700d0fb4
JustSomeGuy420 <alwynsterling@gmail.com>
2025-09-24 19:06:56 -0500

    Added search button function

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-$("#search").click(function() {
-    $.ajax("superheroes.php").done(function(data) {
-        //$("#result").html(data);
-        alert(data);
+$("#search").click(function(element) {
+    element.preventDefault();
+
+    superhero = $("#searchbar").val();
+
+    $.ajax("superheroes.php", {
+        method: "POST",
+        data: { name: superhero }
+    }).done(function(data) {
+        $("#result").html(data);
+    }).fail(function() {
+        alert("An error occurred");
     });
 });
